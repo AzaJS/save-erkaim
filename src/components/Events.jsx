b3b75723f37f23c642fdf6256c6cadaadfe483ea
erkaiymzh <erkaiymzh@gmail.com>
2022-10-03 15:52:15 +0600

    action

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -44,6 +44,35 @@ const Events = () => {
             </Typography>
           </CardContent>
         </Card>
+        <Card
+          className="card"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          sx={{ maxWidth: 345, borderRadius: "30px" }}>
+          <CardMedia
+            component="img"
+            height="200"
+            image="https://i.pinimg.com/564x/54/8f/3d/548f3db1250e743e0d5491a2a1866177.jpg"
+            alt="Акция в поддержку Эркеайым"
+          />
+          <CardContent className="card-content">
+            <Typography
+              sx={{ fontFamily: "Rubik" }}
+              gutterBottom
+              variant="h5"
+              color="white"
+              component="div">
+              Акция "С миру по нитке"
+            </Typography>
+            <Typography
+              sx={{ fontFamily: "Rubik" }}
+              variant="body2"
+              color="white">
+              Помогать может каждый искренне желающий человек, с любого уголка
+              земли! Реквизиты для наших граждан из Турции.
+            </Typography>
+          </CardContent>
+        </Card>
       </div>
       <hr />
       <p className="footer">
